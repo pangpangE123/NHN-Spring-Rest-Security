c771c1615a13ede8c9c0e6bd42cdb266d394ea87
신건영/협업서비스개발레드팀/ND <kunyoung.shin@nhnent.com>
2024-09-27 12:02:14 +0900

    InMemoryUserDetailsManager

--- src/main/java/nhn/academy/config/SecurityConfig.java
+++ src/main/java/nhn/academy/config/SecurityConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,4 +26,15 @@ public class SecurityConfig {
         http.formLogin(Customizer.withDefaults());
         return http.build();
     }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+
 }
